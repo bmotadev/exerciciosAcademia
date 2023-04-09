5f8fe1ed7820a830b371e8ec61fb9b9d1ae2ad9f
Bruno Mota <bmotadev@gmail.com>
2023-04-08 22:26:32 -0300

    :recycle: refactor: organizando exercicios - exercicio 14

--- ExerciciosAcademia/Exercicios/ExerciciosSelecao02.cs
+++ ExerciciosAcademia/Exercicios/ExerciciosSelecao02.cs
@@ -333,7 +333,7 @@ namespace ExerciciosAcademia.Exercicios
              * 14 - Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de Internet (em Mbps). 
              * Em seguida, calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).
              * **/
-            Console.WriteLine("Exercicio 12 - Exercicios Seleção");
+            Console.WriteLine("Exercicio 14 - Exercicios Seleção");
         }
 
         public void Exercicio15()
