cd3fd74f8be231b7fe7bbe7a3b0618c6bbd5a808
Bruno Mota <bmotadev@gmail.com>
2023-04-14 16:48:12 -0300

    :sparkles: feat: Exercicios Matrizes 01 - Resolvido

---
+++ ExerciciosAcademia/Exercicios/ExerciciosMatrizes05.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosAcademia.Exercicios
+{
+    internal class ExerciciosMatrizes05
+    {
+        public void Exercicio01()
+        {
+            /*
+             * 1) Crie uma Matriz 5x3. Na primeira coluna, solicite que o usuário preencha. 
+             * A 2ª coluna, some 10 aos elementos da 1ª coluna. 
+             * Na 3º coluna, armazene o dobro dos elementos da 1ª coluna.
+             * **/
+
+            int[,] matriz = new int[5, 3];
+
+            for (int linha = 0; linha < 5; linha++)
+            {
+                Console.WriteLine("Digite um valor para a primeira coluna:");
+                matriz[linha, 0] = int.Parse(Console.ReadLine());
+                matriz[linha, 1] = matriz[linha, 0] + 10;
+                matriz[linha, 2] = matriz[linha, 0] * 2;
+            }
+
+            for (int linha = 0; linha < 5; linha++) 
+            { 
+                for (int coluna = 0; coluna < 3; coluna++) 
+                { 
+                    Console.Write(matriz[linha, coluna] + "\t"); 
+                } 
+                Console.WriteLine(); 
+            }
+        }
+    }
+}
---
+++ ExerciciosMatrizes05/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ExerciciosMatrizes05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 1) Crie uma Matriz 5x3. Na primeira coluna, solicite que o usuário preencha. 
+             * A 2ª coluna, some 10 aos elementos da 1ª coluna. 
+             * Na 3º coluna, armazene o dobro dos elementos da 1ª coluna.
+             * **/
+        }
+    }
+}
