b08c0856d4c04f1d5c4d216eefe16757a5427daa
Bruno Mota <bmotadev@gmail.com>
2023-04-17 12:36:20 -0300

    :sparkles: feat:  Exercicios Matrizes 02 - Resolvido

--- ExerciciosAcademia/Exercicios/ExerciciosMatrizes05.cs
+++ ExerciciosAcademia/Exercicios/ExerciciosMatrizes05.cs
@@ -49,6 +49,55 @@ namespace ExerciciosAcademia.Exercicios
 	                    -Ex: Coluna 1: 23
 	                         Coluna 2: 36
              * **/
+
+            int[,] matriz = new int[2, 3];
+            Console.WriteLine(matriz.GetLength(0)); //mostra qtas linhas ha
+            Console.WriteLine(matriz.GetLength(1)); //mostra qtas colunas ha
+
+            Console.WriteLine("Preencha a matriz 2x3!!");
+
+            for (int linha = 0; linha < matriz.GetLength(0); linha++)
+            {
+                for (int coluna = 0; coluna < matriz.GetLength(1); coluna++)
+                {
+                    Console.Write("Posicao[" + linha + "," + coluna + "]: ");
+                    matriz[linha, coluna] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            //exibir a matriz
+            for (int linha = 0; linha < matriz.GetLength(0); linha++)
+            {
+                for (int coluna = 0; coluna < matriz.GetLength(1); coluna++)
+                {
+                    Console.Write(matriz[linha, coluna] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            int soma;
+            //calcular e mostrar a soma de linha a linha
+            for (int linha = 0; linha < matriz.GetLength(0); linha++)
+            {
+                soma = 0;
+                for (int coluna = 0; coluna < matriz.GetLength(1); coluna++)
+                {
+                    soma = soma + matriz[linha, coluna];
+                }
+                Console.WriteLine("A soma dos elementos da linha " + linha + ": " + soma);
+            }
+
+
+            //calcular e mostrar a soma de coluna a coluna
+            for (int coluna = 0; coluna < matriz.GetLength(1); coluna++)
+            {
+                soma = 0;
+                for (int linha = 0; linha < matriz.GetLength(0); linha++)
+                {
+                    soma = soma + matriz[linha, coluna];
+                }
+                Console.WriteLine("A soma dos elementos da coluna " + coluna + ": " + soma);
+            }
         }
 
         public void Exercicio03()
