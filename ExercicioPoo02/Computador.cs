87bdcf8c0a97ab0a8e050bf7f483179cad744c1c
Bruno Mota <bmotadev@gmail.com>
2023-05-02 16:00:38 -0300

    :sparkles: feat: Exercicio 02 - POO - Concluido

---
+++ ExercicioPoo02/Computador.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioPoo02
+{
+    internal class Computador
+    {
+        //•Crie uma classe chamada Computador que possua 4 atributos: marca, modelo, tipo(notebook, netbook, tablet, etc) e preço.
+
+        //Crie dois objetos onde o primeiro objeto deverá ser criado através da solicitação dos valores ao usuário por linha de execução.Já, os valores do segundo objeto, deverão ser definidos no código - fonte da classe que o criou.Exiba na tela os resultados.
+
+        public string marca;
+        public string modelo;
+        public string tipo;
+        public double preco;
+
+        public Computador()
+        {
+
+        }
+
+        public Computador(string marca, string modelo, double preco, string tipo)
+        {
+            this.marca = marca;
+            this.modelo = modelo;
+            this.preco = preco;
+            this.tipo = tipo;
+        }
+
+        public void exibeDados()
+        {
+            Console.WriteLine($"Marca: {this.marca}");
+            Console.WriteLine($"Modelo: {this.modelo}");
+            Console.WriteLine($"Tipo: {this.tipo}");
+            Console.WriteLine($"Preço: {this.preco:f2}");
+        }
+    }
+}
---
+++ ExercicioPoo02/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using System.Runtime.ConstrainedExecution;
+using System.Text.RegularExpressions;
+using System;
+
+namespace ExercicioPoo02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //•Crie uma classe chamada Computador que possua 4 atributos: marca, modelo, tipo(notebook, netbook, tablet, etc) e preço.
+
+            //Crie dois objetos onde o primeiro objeto deverá ser criado através da solicitação dos valores ao usuário por linha de execução.Já, os valores do segundo objeto, deverão ser definidos no código - fonte da classe que o criou.Exiba na tela os resultados.
+
+            Computador computador = new Computador();
+            Computador computador2 = new Computador();
+
+            Console.WriteLine("Informe a marca:");
+            computador.marca = Console.ReadLine();
+
+            Console.WriteLine("Informe o modelo: ");
+            computador.modelo = Console.ReadLine();
+
+            Console.WriteLine("Informe o tipo: ");
+            computador.tipo = Console.ReadLine();
+
+            Console.WriteLine("Informe o valor: ");
+            computador.preco = double.Parse(Console.ReadLine());
+
+            computador2.marca = "Positivo";
+            computador2.modelo = "G320";
+            computador2.tipo = "Notebook";
+            computador2.preco = 2800;
+
+            Console.WriteLine("Computador 1");
+            computador.exibeDados();
+
+            Console.WriteLine("Computador 2");
+            computador2.exibeDados();
+        }
+    }
+}
