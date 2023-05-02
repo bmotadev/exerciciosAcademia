8bf665894eabed878424f7033cc2683192f8061e
Bruno Mota <bmotadev@gmail.com>
2023-05-02 15:38:39 -0300

    :sparkles: feat: Exercicio 01 - POO - Carro - Concluido

---
+++ ExercicioPoo/Carro.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioPoo
+{
+    internal class Carro
+    {
+        //•1 - Crie uma classe chamada Carro que possua 3 atributos: marca, modelo e anoFabricacao.Além disso, na main crie 2 objetos do tipo Carro e exiba na tela os valores dos atributos criados.
+
+        //•Esses valores devem ser solicitados ao usuário, por linha de execução.
+
+        public string marca;
+        public string modelo;
+        public string anoFabricacao;
+
+        public Carro()
+        {
+        }
+
+        public Carro(string marca, string modelo, string anoFabricacao)
+        {
+            this.marca = marca;
+            this.modelo = modelo;
+            this.anoFabricacao = anoFabricacao;
+        }
+
+        public void exibeDados()
+        {
+            Console.WriteLine($"Marca: {this.marca}");
+            Console.WriteLine($"Modelo: {this.modelo}");
+            Console.WriteLine($"Ano de Fabricação: {this.anoFabricacao}");
+        }
+    }
+}
---
+++ ExercicioPoo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection;
+using System.Runtime.Intrinsics.X86;
+using System.Text.RegularExpressions;
+using System.Threading.Channels;
+
+namespace ExercicioPoo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //•1 - Crie uma classe chamada Carro que possua 3 atributos: marca, modelo e anoFabricacao.Além disso, na main crie 2 objetos do tipo Carro e exiba na tela os valores dos atributos criados.
+
+            //•Esses valores devem ser solicitados ao usuário, por linha de execução.
+
+            Carro carro = new Carro();
+            Carro carro2 = new Carro();
+
+            Console.WriteLine("Informe a marca");
+            carro.marca = Console.ReadLine();
+
+            Console.WriteLine("Informe o modelo:");
+            carro.modelo = Console.ReadLine();
+
+            Console.WriteLine("Informe o ano de fabricação:");
+            carro.anoFabricacao = Console.ReadLine();
+
+
+            Console.WriteLine("Informe a marca");
+            carro2.marca = Console.ReadLine();
+
+            Console.WriteLine("Informe o modelo:");
+            carro2.modelo = Console.ReadLine();
+
+            Console.WriteLine("Informe o ano de fabricação:");
+            carro2.anoFabricacao = Console.ReadLine();
+
+
+            Console.WriteLine("Carro 1");
+            carro.exibeDados();
+
+            Console.WriteLine("Carro 2");
+            carro2.exibeDados();
+
+        }
+    }
+}
