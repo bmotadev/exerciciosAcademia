06fb66662f57ea7648f254320d9ea7792546fe54
Bruno Mota <bmotadev@gmail.com>
2023-05-05 16:11:58 -0300

    :sparkles: feat: Exercicio de Unidade de Saude com POO

---
+++ ExercicioPooUpaSaude/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using System.Runtime.Intrinsics.X86;
+
+namespace ExercicioPooUpaSaude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Uma Unidade de Saúde, ao receber pessoas para consultas, realiza cadastro do paciente(nome, cpf, dataNascimento, email).Feito isso, o paciente cadastrado participa de um processo de triagem(valida dados do paciente,
+            //peso, altura, pressão arterial e temperatura). Em cima desses dados o técnico ou enfermeiro colocam a urgência
+            //de atendimento(verde, amarelo, vermelho).Feito isso, o paciente é chamado a consulta, em que são guardados:
+            //-paciente
+            //- triagem
+            //- nome e crm do médico
+            //- prescrição gerada
+            //UnidadeSaude
+            //Paciente
+            //Triagem
+            //Consulta
+            //ProfissionalSaude
+        }
+    }
+}
