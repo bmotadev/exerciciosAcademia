c2ddc76389c97a64b85c77485f2fb733201f62bd
Bruno Mota <bmotadev@gmail.com>
2023-05-15 15:56:19 -0300

    :sparkles: feat: Exercicio POO Veiculo

---
+++ ExercicioPooVeiculo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Text.RegularExpressions;
+using System;
+
+namespace ExercicioPooVeiculo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criar uma classe Veiculo que contém as propriedades Marca e Modelo.Criar duas classes derivadas, Carro e Moto, que herdam de Veiculo.A classe Carro deve ter uma propriedade adicional QuantidadeDePortas, enquanto a classe Moto deve ter uma propriedade adicional Cilindrada. Criar um método Dirigir na classe Veiculo que imprime "Dirigindo o <marca> <modelo>".Em seguida, criar um método Dirigir na classe Carro que imprime "Dirigindo o <marca> <modelo> com <quantidadeDePortas> portas" e um método Dirigir na classe Moto que imprime "Dirigindo a <marca> <modelo> com <cilindrada> cilindradas".
+        }
+    }
+}
