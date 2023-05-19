751e3eca4dff021408d41f535adff76005558290
Bruno Mota <bmotadev@gmail.com>
2023-05-19 14:39:34 -0300

    :sparkles: feat: Aula 19 de Maio de 2023 - Exercicio de Leitura de Glicemia

---
+++ ProgramaLeitorGlicemia/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProgramaLeitorGlicemia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // solicitar para o usuario o nome e o caminho do arquivo
+        }
+    }
+}
